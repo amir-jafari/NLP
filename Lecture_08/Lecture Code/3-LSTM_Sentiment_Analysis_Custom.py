84a2db5e0fa555f98f1d37bf8ea44f6597870a08
amir-jafari <amir.h.jafari@okstate.edu>
2022-11-22 15:46:25 -0500

    Updated

--- Lecture_08/Lecture Code/3-LSTM_Sentiment_Analysis_Custom.py
+++ Lecture_08/Lecture Code/3-LSTM_Sentiment_Analysis_Custom.py
@@ -1,22 +1,14 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from nltk.corpus import stopwords
 from collections import Counter
-import string
 import re
-import seaborn as sns
-from tqdm import tqdm
-import matplotlib.pyplot as plt
 from torch.utils.data import TensorDataset, DataLoader
 from sklearn.model_selection import train_test_split
 
-
 is_cuda = torch.cuda.is_available()
-
-# If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
 if is_cuda:
     device = torch.device("cuda")
     print("GPU is available")
@@ -24,10 +16,7 @@ else:
     device = torch.device("cpu")
     print("GPU not available, CPU used")
 
-
-base_csv = 'IMDB Dataset.csv'
-df = pd.read_csv(base_csv)
-df.head()
+df = pd.read_csv('IMDB Dataset.csv')
 
 X,y = df['review'].values,df['sentiment'].values
 x_train,x_test,y_train,y_test = train_test_split(X,y,stratify=y)
@@ -36,19 +25,13 @@ print(f'shape of test data is {x_test.shape}')
 
 
 def preprocess_string(s):
-    # Remove all non-word characters (everything except numbers and letters)
     s = re.sub(r"[^\w\s]", '', s)
-    # Replace all runs of whitespaces with no space
     s = re.sub(r"\s+", '', s)
-    # replace digits with no space
     s = re.sub(r"\d", '', s)
-
     return s
-
 
 def tockenize(x_train, y_train, x_val, y_val):
     word_list = []
-
     stop_words = set(stopwords.words('english'))
     for sent in x_train:
         for word in sent.lower().split():
@@ -57,12 +40,9 @@ def tockenize(x_train, y_train, x_val, y_val):
                 word_list.append(word)
 
     corpus = Counter(word_list)
-    # sorting on the basis of most common words
     corpus_ = sorted(corpus, key=corpus.get, reverse=True)[:1000]
-    # creating a dict
     onehot_dict = {w: i + 1 for i, w in enumerate(corpus_)}
 
-    # tockenize
     final_list_train, final_list_test = [], []
     for sent in x_train:
         final_list_train.append([onehot_dict[preprocess_string(word)] for word in sent.lower().split()
@@ -77,7 +57,6 @@ def tockenize(x_train, y_train, x_val, y_val):
         encoded_test), onehot_dict
 
 x_train,y_train,x_test,y_test,vocab = tockenize(x_train,y_train,x_test,y_test)
-print(f'Length of vocabulary is {len(vocab)}')
 
 def padding_(sentences, seq_len):
     features = np.zeros((len(sentences), seq_len),dtype=int)
@@ -86,27 +65,18 @@ def padding_(sentences, seq_len):
             features[ii, -len(review):] = np.array(review)[:seq_len]
     return features
 
-x_train_pad = padding_(x_train,500)
-x_test_pad = padding_(x_test,500)
+x_train_pad = padding_(x_train,200)
+x_test_pad = padding_(x_test,200)
 
-# create Tensor datasets
+
 train_data = TensorDataset(torch.from_numpy(x_train_pad), torch.from_numpy(y_train))
 valid_data = TensorDataset(torch.from_numpy(x_test_pad), torch.from_numpy(y_test))
 
-# dataloaders
-batch_size = 50
 
-# make sure to SHUFFLE your data
+batch_size = 10
+
 train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
 valid_loader = DataLoader(valid_data, shuffle=True, batch_size=batch_size)
-
-# obtain one batch of training data
-dataiter = iter(train_loader)
-sample_x, sample_y = dataiter.next()
-
-print('Sample input size: ', sample_x.size()) # batch_size, seq_length
-print('Sample input: \n', sample_x)
-print('Sample input: \n', sample_y)
 
 
 class SentimentRNN(nn.Module):
@@ -118,85 +88,50 @@ class SentimentRNN(nn.Module):
 
         self.no_layers = no_layers
         self.vocab_size = vocab_size
-
-        # embedding and LSTM layers
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-
-        # lstm
-        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=self.hidden_dim,
-                            num_layers=no_layers, batch_first=True)
-
-        # dropout layer
+        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=self.hidden_dim, num_layers=no_layers, batch_first=True)
         self.dropout = nn.Dropout(0.3)
-
-        # linear and sigmoid layer
         self.fc = nn.Linear(self.hidden_dim, output_dim)
         self.sig = nn.Sigmoid()
 
     def forward(self, x, hidden):
         batch_size = x.size(0)
-        # embeddings and lstm_out
-        embeds = self.embedding(x)  # shape: B x S x Feature   since batch = True
-        # print(embeds.shape)  #[50, 500, 1000]
+        embeds = self.embedding(x) 
         lstm_out, hidden = self.lstm(embeds, hidden)
-
         lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
-
-        # dropout and fully connected layer
         out = self.dropout(lstm_out)
         out = self.fc(out)
-
-        # sigmoid function
         sig_out = self.sig(out)
-
-        # reshape to be batch_size first
         sig_out = sig_out.view(batch_size, -1)
-
-        sig_out = sig_out[:, -1]  # get last batch of labels
-
-        # return last sigmoid output and hidden state
+        sig_out = sig_out[:, -1]
         return sig_out, hidden
 
     def init_hidden(self, batch_size):
-        ''' Initializes hidden state '''
-        # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
-        # initialized to zero, for hidden state and cell state of LSTM
         h0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
         c0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
         hidden = (h0, c0)
         return hidden
 
 no_layers = 2
-vocab_size = len(vocab) + 1 #extra 1 for padding
+vocab_size = len(vocab) + 1
 embedding_dim = 64
 output_dim = 1
 hidden_dim = 256
 
 
 model = SentimentRNN(no_layers,vocab_size,hidden_dim,embedding_dim,drop_prob=0.5)
-
-#moving to gpu
 model.to(device)
-
-print(model)
-
-# loss and optimization functions
 lr=0.001
-
 criterion = nn.BCELoss()
-
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-# function to predict accuracy
 def acc(pred,label):
     pred = torch.round(pred.squeeze())
     return torch.sum(pred == label.squeeze()).item()
 
-
 clip = 5
 epochs = 5
 valid_loss_min = np.Inf
-# train for some number of epochs
 epoch_tr_loss, epoch_vl_loss = [], []
 epoch_tr_acc, epoch_vl_acc = [], []
 
@@ -204,25 +139,17 @@ for epoch in range(epochs):
     train_losses = []
     train_acc = 0.0
     model.train()
-    # initialize hidden state
     h = model.init_hidden(batch_size)
     for inputs, labels in train_loader:
         inputs, labels = inputs.to(device), labels.to(device)
-        # Creating new variables for the hidden state, otherwise
-        # we'd backprop through the entire training history
         h = tuple([each.data for each in h])
-
         model.zero_grad()
         output, h = model(inputs, h)
-
-        # calculate the loss and perform backprop
         loss = criterion(output.squeeze(), labels.float())
         loss.backward()
         train_losses.append(loss.item())
-        # calculating accuracy
         accuracy = acc(output, labels)
         train_acc += accuracy
-        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
         nn.utils.clip_grad_norm_(model.parameters(), clip)
         optimizer.step()
 
@@ -232,14 +159,10 @@ for epoch in range(epochs):
     model.eval()
     for inputs, labels in valid_loader:
         val_h = tuple([each.data for each in val_h])
-
         inputs, labels = inputs.to(device), labels.to(device)
-
         output, val_h = model(inputs, val_h)
         val_loss = criterion(output.squeeze(), labels.float())
-
         val_losses.append(val_loss.item())
-
         accuracy = acc(output, labels)
         val_acc += accuracy
 
@@ -254,8 +177,3 @@ for epoch in range(epochs):
     print(f'Epoch {epoch + 1}')
     print(f'train_loss : {epoch_train_loss} val_loss : {epoch_val_loss}')
     print(f'train_accuracy : {epoch_train_acc * 100} val_accuracy : {epoch_val_acc * 100}')
-    if epoch_val_loss <= valid_loss_min:
-        torch.save(model.state_dict(), 'state_dict.pt')
-        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_loss_min, epoch_val_loss))
-        valid_loss_min = epoch_val_loss
-    print(25 * '==')
