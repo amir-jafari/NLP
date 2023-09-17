17ac3723f7f40ce93a8392f98d6325a0626175db
amir-jafari <amir.h.jafari@okstate.edu>
2023-09-17 18:49:42 -0400

    generate 10 words in LM

--- Lecture_01/Lecture Code/19_runner.py
+++ Lecture_01/Lecture Code/19_runner.py
@@ -1,5 +1,5 @@
 import os
-os.system('python3 19_positional_arg.py Amir 99')
+os.system('python3 19_positional_arg.py Amir 16')
 os.system('python3 19_type_arg.py -n 3')
 
 
--- Lecture_02/Lecture Code/14_Generate_Random_Bigram.py
+++ Lecture_02/Lecture Code/14_Generate_Random_Bigram.py
@@ -3,7 +3,7 @@ sent = ['In', 'the', 'beginning', 'God', 'created',
         'the', 'heaven', 'and', 'the', 'earth', '.']
 print(list(nltk.bigrams(sent)))
 
-def generate_model(cfdist, word, num=15):
+def generate_model(cfdist, word, num=10):
     for i in range(num):
         print(word, end=' ')
         word = cfdist[word].max()
