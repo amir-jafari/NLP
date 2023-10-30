08bfe9499b76ebed422c4b6b4eae01a5099d81a3
amir-jafari <amir.h.jafari@okstate.edu>
2023-10-30 09:36:07 -0400

    Modify Sample code

--- Lecture_07/Lecture Code/8-Sample_Code.py
+++ Lecture_07/Lecture Code/8-Sample_Code.py
@@ -1,62 +1,102 @@
 import torch
 from torch import nn
-from torchtext.data.utils import get_tokenizer
-from torchtext.vocab import build_vocab_from_iterator
-from torch.utils.data import DataLoader
-from torchtext.datasets import AG_NEWS
+from torch.utils.data import DataLoader, Dataset
 import time
-# ------------------------------------------------------------------------------
-train_iter = list(AG_NEWS(split='train'))
-test_iter = list(AG_NEWS(split='test'))
-print(train_iter[0])
-# ------------------------------------------------------------------------------
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-tokenizer = get_tokenizer('basic_english')
-def yield_tokens(data_iter):
-    for _, text in data_iter:
-        yield tokenizer(text)
+import os
+import csv
 
-vocab = build_vocab_from_iterator(yield_tokens(train_iter), specials=["<unk>"])
-vocab.set_default_index(vocab["<unk>"])
-print(vocab(['here', 'is', 'an', 'example']))
-# ------------------------------------------------------------------------------
-text_pipeline = lambda x: vocab(tokenizer(x))
-label_pipeline = lambda x: int(x) - 1
-print(text_pipeline('here is the an example'))
-print(label_pipeline('10'))
-# ------------------------------------------------------------------------------
+# Define a custom dataset class
+class AGNewsDataset(Dataset):
+    def __init__(self, split, transform=None):
+        assert split in ['train', 'test']
+        self.data = self.load_data(split)
+        self.transform = transform
+
+    def load_data(self, split):
+        data = []
+        csv_file = f'{split}.csv'
+        with open(csv_file, 'r', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                label, text, _ = row
+                try:
+                    data.append((int(label), text))
+                except:
+                    pass
+
+        return data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        label, text = self.data[idx]
+        return label, text
+
+
+def label_pipeline(label):
+    return label - 1
+
+
+def text_pipeline(text):
+    # You can implement custom text preprocessing here if needed
+    # For this example, we'll just split the text into tokens
+    tokens = text.split()
+    token_ids = [vocab.get(token, vocab['<unk>']) for token in tokens]
+    return torch.tensor(token_ids, dtype=torch.int64)
+
+
+
+
+# Create data loaders
+
+
+# Define your vocabulary if necessary
+vocab = {}  # You can build your vocabulary here if needed
+# Define text and label processing functions
+vocab['<unk>'] = len(vocab)  # Add <unk> token
+
+# Create a DataLoader and collate function
 def collate_batch(batch):
-    label_list, text_list, offsets = [], [], [0]
-    for (_label, _text) in batch:
-         label_list.append(label_pipeline(_label))
-         processed_text = torch.tensor(text_pipeline(_text), dtype=torch.int64)
-         text_list.append(processed_text)
-         offsets.append(processed_text.size(0))
-    label_list = torch.tensor(label_list, dtype=torch.int64)
-    offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
-    text_list = torch.cat(text_list)
-    return label_list.to(device), text_list.to(device), offsets.to(device)
-# ------------------------------------------------------------------------------
+    labels, texts = zip(*batch)
+    labels = torch.tensor([label_pipeline(label) for label in labels], dtype=torch.int64)
+    texts = [text_pipeline(text) for text in texts]
+    offsets = [0] + [len(text) for text in texts[:-1]]
+    offsets = torch.tensor(offsets, dtype=torch.int64).cumsum(dim=0)
+    texts = torch.cat(texts)
+    return labels.to(device), texts.to(device), offsets.to(device)
+
+# Define the TextClassificationModel
 class TextClassificationModel(nn.Module):
     def __init__(self, vocab_size, embed_dim, num_class):
         super(TextClassificationModel, self).__init__()
         self.embedding = nn.EmbeddingBag(vocab_size, embed_dim)
         self.fc = nn.Linear(embed_dim, num_class)
-        self.init_weights()
-    def init_weights(self):
-        initrange = 0.5
-        self.embedding.weight.data.uniform_(-initrange, initrange)
-        self.fc.weight.data.uniform_(-initrange, initrange)
-        self.fc.bias.data.zero_()
+
     def forward(self, text, offsets):
         embedded = self.embedding(text, offsets)
         return self.fc(embedded)
-# ------------------------------------------------------------------------------
-num_class = len(set([label for (label, text) in train_iter]))
+
+
+EPOCHS = 100
+LR = 0.001
+BATCH_SIZE = 64
+emsize = 128
+
+# Load the AG_NEWS dataset
+train_dataset = AGNewsDataset(split='train', transform=text_pipeline)
+test_dataset = AGNewsDataset(split='test', transform=text_pipeline)
+
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_batch)
+test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_batch)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+num_class = len(set([label for (label,_,_) in train_dataloader]))
 vocab_size = len(vocab)
-emsize = 64
+
 model = TextClassificationModel(vocab_size, emsize, num_class).to(device)
-# ------------------------------------------------------------------------------
+
+# Define the training and evaluation functions
 def train(dataloader):
     model.train()
     total_acc, total_count = 0, 0
@@ -68,15 +108,12 @@ def train(dataloader):
         predicted_label = model(text, offsets)
         loss = criterion(predicted_label, label)
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
         optimizer.step()
         total_acc += (predicted_label.argmax(1) == label).sum().item()
         total_count += label.size(0)
         if idx % log_interval == 0 and idx > 0:
             elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches '
-                  '| accuracy {:8.3f}'.format(epoch, idx, len(dataloader),
-                                              total_acc/total_count))
+            print('| epoch {:3d} | {:5d}/{:5d} batches | accuracy {:8.3f}'.format(epoch, idx, len(dataloader), total_acc/total_count))
             total_acc, total_count = 0, 0
             start_time = time.time()
 
@@ -91,30 +128,23 @@ def evaluate(dataloader):
             total_acc += (predicted_label.argmax(1) == label).sum().item()
             total_count += label.size(0)
     return total_acc/total_count
-# ------------------------------------------------------------------------------
-EPOCHS = 10
-LR = 0.001
-BATCH_SIZE = 64
+
+# Set training parameters
+
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=LR)
-# ------------------------------------------------------------------------------
-train_dataloader = DataLoader(train_iter, batch_size=BATCH_SIZE,
-                              shuffle=True, collate_fn=collate_batch)
-test_dataloader = DataLoader(test_iter, batch_size=BATCH_SIZE,
-                              shuffle=True, collate_fn=collate_batch)
 
-
+# Training loop
 for epoch in range(1, EPOCHS + 1):
     epoch_start_time = time.time()
     train(train_dataloader)
     accu_val = evaluate(test_dataloader)
     print('-' * 59)
-    print('| end of epoch {:3d} | time: {:5.2f}s | '
-          'valid accuracy {:8.3f} '.format(epoch,
-                                           time.time() - epoch_start_time, accu_val))
+    print('| end of epoch {:3d} | time: {:5.2f}s | valid accuracy {:8.3f} '.format(epoch, time.time() - epoch_start_time, accu_val))
     print('-' * 59)
-# ------------------------------------------------------------------------------
+
+# Save the trained model
 torch.save(model.state_dict(), 'model_weights.pt')
-# load the model first for inference
+# Load the model for inference
 model.load_state_dict(torch.load('model_weights.pt'))
-model.eval()
+
