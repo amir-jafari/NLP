5a5b43e46e61f6244cbacf5ef386533550ab45d3
Amir <amir.h.jafari@okstate.edu>
2024-02-13 09:10:27 -0500

    Fix Regx

--- Lecture_04/Lecture Code/11_Example_Quantifier_Email.py
+++ Lecture_04/Lecture Code/11_Example_Quantifier_Email.py
@@ -3,19 +3,19 @@ with open('re_email.txt','r') as f:
     text = f.read()
 # ----------------------------
 print(20 * '-' + 'Example - email' + 20 * '-' )
-pattern1 = re.compile(r'[a-zA-z]+@[a-zA-z]+\.com')
+pattern1 = re.compile(r'[a-zA-Z]+@[a-zA-Z]+\.com')
 matches = pattern1.finditer(text)
 for match in matches:
     print(match)
 # # # ----------------------------
 print(20 * '-' + 'Example - email' + 20 * '-' )
-pattern2 = re.compile(r'[a-zA-z.]+@[a-zA-z]+\.(com|edu)')
+pattern2 = re.compile(r'[a-zA-Z.]+@[a-zA-Z]+\.(com|edu)')
 matches = pattern2.finditer(text)
 for match in matches:
     print(match)
 # # # # # ----------------------------
 print(20 * '-' + 'Example - email' + 20 * '-' )
-pattern3 = re.compile(r'[a-zA-z0-9.-]+@[a-zA-z-]+\.(com|edu|net)')
+pattern3 = re.compile(r'[a-zA-Z0-9.-]+@[a-zA-Z-]+\.(com|edu|net)')
 matches = pattern3.finditer(text)
 for match in matches:
     print(match)
