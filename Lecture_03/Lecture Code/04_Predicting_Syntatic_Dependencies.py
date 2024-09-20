e9e622bf78d22f661a9b41f58d2924bb5082c072
Amir <amir.h.jafari@okstate.edu>
2024-09-20 11:22:54 -0400

    Add sentiment analysis using VaderSentiment
    Integrated VaderSentiment to perform sentiment analysis on text. Added a new example demonstrating the sentiment scoring of a sample text. This enhances the script by combining syntactic dependency parsing with sentiment analysis.

--- Lecture_03/Lecture Code/04_Predicting_Syntatic_Dependencies.py
+++ Lecture_03/Lecture Code/04_Predicting_Syntatic_Dependencies.py
@@ -1,8 +1,15 @@
 # https://spacy.io/usage/linguistic-features
 import spacy
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 nlp = spacy.load("en_core_web_sm")
 doc = nlp("She ate the pizza")
 for token in doc:
     print(token.text, token.pos_, token.dep_, token.head.text)
 
+
+analyzer = SentimentIntensityAnalyzer()
+text = "This is the best movie I have ever seen. Amazing experience!"
+doc = nlp(text)
+sentiment = analyzer.polarity_scores(doc.text)
+print(sentiment)
