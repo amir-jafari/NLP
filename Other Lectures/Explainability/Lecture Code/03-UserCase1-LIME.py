1f4a9c1d9e6ad51bef2153d2a7bec80ad8967ca9
Yongpu (Aaron) Yang <aaron.young@gwu.edu>
2025-02-18 21:30:06 -0500

    make changes to lecture code

--- Other Lectures/Explainability/Lecture Code/03-UserCase1-LIME.py
+++ Other Lectures/Explainability/Lecture Code/03-UserCase1-LIME.py
@@ -8,6 +8,8 @@
 # The dataset 'IMDb' is used in this user case. Small Part of 'IMDb' is selected
 # for sentiment Analysis, and the goal is to classify reviews as positive or
 # negative.
+#
+# This py file is to use the HandExplainer and the Lime Package separately.
 # ==========================================================================
 
 import pandas as pd
@@ -15,4 +17,12 @@ import pandas as pd
 df_train = pd.read_csv('imdb_train.csv')
 df_test = pd.read_csv('imdb_test.csv')
 
+# ==========================================================================
+# Use LIMEHandExplainer built by ourself
+# ==========================================================================
 
+
+
+# ==========================================================================
+# Use LIME package
+# ==========================================================================
