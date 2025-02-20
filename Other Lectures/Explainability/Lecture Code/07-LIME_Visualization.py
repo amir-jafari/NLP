9c0c7f09e744d674bfaf5ed238664951c19b8ade
Yongpu (Aaron) Yang <aaron.young@gwu.edu>
2025-02-20 17:57:51 -0500

    make changes to lecture code

--- Other Lectures/Explainability/Lecture Code/07-LIME_Visualization.py
+++ Other Lectures/Explainability/Lecture Code/07-LIME_Visualization.py
@@ -50,3 +50,26 @@ plt.xlabel("Weight")
 plt.gca().invert_yaxis()
 plt.tight_layout()
 plt.show()
+
+
+#%%
+import shap
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(r'C:\Users\aaron\PycharmProjects\NLP\Other Lectures\Explainability\Lecture Code\Customer Churn.csv')
+df.head()
+# Dropping all irrelevant columns
+df.drop(columns=['customerID'], inplace = True)
+
+df.dropna(inplace=True)
+
+#%%
+
+# Label Encoding features
+categorical_feat =list(df.select_dtypes(include=["object"]))
+
+# Using label encoder to transform string categories to integer labels
+le = LabelEncoder()
+for feat in categorical_feat:
+    df[feat] = le.fit_transform(df[feat]).astype('int')
