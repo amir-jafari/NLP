efd972a34e903412856ee8b00f0b0ca2cab010b3
Aaron(Yongpu Yang) <143126371+AARONYOUNG2023@users.noreply.github.com>
2025-02-27 03:36:58 -0500

    lecture code changed

---
+++ Other Lectures/Hugging Face/Lecture Code/7-Hugging_Face_decoder_gpt2.py
@@ -0,0 +1,16 @@
+#%% --------------------------------------------------------------------------------------------------------------------
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import torch
+
+#%% --------------------------------------------------------------------------------------------------------------------
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+#%% --------------------------------------------------------------------------------------------------------------------
+prompt = 'Transformers are'
+input_ids = tokenizer.encode(prompt, return_tensors='pt')
+
+#%% --------------------------------------------------------------------------------------------------------------------
+output_ids = model.generate(input_ids, max_length=30, num_beams=2, no_repeat_ngram_size=2)
+generated_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+print(generated_text)
--- Other Lectures/Hugging Face/Lecture Code/8-Hugging_Face_T5.py
+++ Other Lectures/Hugging Face/Lecture Code/8-Hugging_Face_T5.py
@@ -1,15 +1,15 @@
 #%% --------------------------------------------------------------------------------------------------------------------
-from transformers import BartTokenizer, BartForConditionalGeneration
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 #%% --------------------------------------------------------------------------------------------------------------------
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
-model = BartForConditionalGeneration.from_pretrained('facebook/bart-base')
+tokenizer = T5Tokenizer.from_pretrained('t5-small')
+model = T5ForConditionalGeneration.from_pretrained('t5-small')
 
 #%% --------------------------------------------------------------------------------------------------------------------
-input_text = 'Bart is a powerful seq2seq model developed by Facebook AI.'
-inputs = tokenizer.encode(input_text, return_tensors='pt')
+input_text = 'summarize: Encoder-decoder models are widely used.'
+input_ids = tokenizer.encode(input_text, return_tensors='pt')
 
 #%% --------------------------------------------------------------------------------------------------------------------
-summary_ids = model.generate(inputs, max_length=30, num_beams=4)
+summary_ids = model.generate(input_ids, max_length=30, num_beams=2)
 summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 print(summary)
