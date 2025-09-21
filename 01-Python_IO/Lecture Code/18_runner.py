f20d636d0078c05fb106584687fa5a71cf9acc49
Amir <amir.h.jafari@okstate.edu>
2025-09-21 18:12:38 -0400

    Update SpaCy model to `en_core_web_sm` and anonymize script arguments
    Replaced `en_core_web_md` with a smaller SpaCy model (`en_core_web_sm`) for resource efficiency. Updated script argument to use a placeholder (`XXXXX`) to avoid hard-coded personal data.

--- 01-Python_IO/Lecture Code/18_runner.py
+++ 01-Python_IO/Lecture Code/18_runner.py
@@ -1,5 +1,5 @@
 import os
-os.system('python3 18_required_arg.py --name Amir')
+os.system('python3 18_required_arg.py --name XXXXX')
 os.system('python3 18_optional_arg.py -o')
 os.system('python3 18_optional_arg.py --output')
 
--- 03-SpaCy/Lecture Code/19_Word_Vectors.py
+++ 03-SpaCy/Lecture Code/19_Word_Vectors.py
@@ -1,5 +1,5 @@
 import spacy
-nlp = spacy.load("en_core_web_md")
+nlp = spacy.load("en_core_web_sm")
 
 doc = nlp("I have a banana")
 print(doc[3].vector)
